c7b528125461785fc9ea2cff72e4c91826da46c2
Ricardo Wagner <114697430+Ricardo-Wgnr@users.noreply.github.com>
2025-07-16 11:29:04 -0300

    Add files via upload

--- Projeto IIR/questao1.m
+++ Projeto IIR/questao1.m
@@ -9,3 +9,8 @@ title('Resposta em frequência do filtro Butterworth');
 disp(Num);
 disp(Den);
 
+b = [0.00243];
+a = [1 1.417 1.003 0.451 0.135 0.0256 0.00243];
+fs = 1/2;
+[bd,ad] = bilinear(b,a,fs)
+
---
+++ Projeto IIR/questao2eliptico.m
@@ -0,0 +1,58 @@
+close all
+
+load('coeflow2.mat');
+load('coefhigh2.mat');
+load('coefmid2.mat');
+%% Sinal 
+tmin = 0;             
+tmax = 2;
+Fs=8000;
+Ts=1/Fs;
+L=(tmax-tmin)/Ts;
+
+t=0:Ts:tmax-Ts;             
+s = sin(2*pi*770*t) + sin(2*pi*852*t) + sin(2*pi*941*t);
+S = fft(s);
+S = abs(2*S/L);
+S = fftshift(S);  
+
+%% Filtros IIR importados do filterDesigner
+figure,freqz(num_low,den_low);
+ 
+s_f_low = filter(num_low,den_low,s);
+S_F_low = fft(s_f_low);
+S_F_low = abs(2*S_F_low/L);
+S_F_low=fftshift(S_F_low);  
+
+figure,freqz(num_high,den_high);
+ 
+s_f_high = filter(num_high,den_high,s);
+S_F_high = fft(s_f_high);
+S_F_high = abs(2*S_F_high/L);
+S_F_high=fftshift(S_F_high);      
+
+figure,freqz(num_high,den_high);
+ 
+s_f_mid = filter(num_mid,den_mid,s);
+S_F_mid = fft(s_f_mid);
+S_F_mid = abs(2*S_F_mid/L);
+S_F_mid=fftshift(S_F_mid); 
+
+%% Gráficos
+freq = Fs*(-(L/2):(L/2)-1)/L;
+subplot(4,1,1),plot(freq,S)
+title('Espectro de Amplitude de s(t)')
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+subplot(4,1,2),plot(freq,S_F_low)
+title('Espectro de Amplitude do sinal Filtrado - baixa frequência')
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+subplot(4,1,3),plot(freq,S_F_high)
+title('Espectro de Amplitude do sinal Filtrado - alta frequência')
+xlabel('f (Hz)')
+ylabel('|S(f)|')
+subplot(4,1,4),plot(freq,S_F_mid)
+title('Espectro de Amplitude do sinal Filtrado - frequência intermediária')
+xlabel('f (Hz)')
+ylabel('|S(f)|')
